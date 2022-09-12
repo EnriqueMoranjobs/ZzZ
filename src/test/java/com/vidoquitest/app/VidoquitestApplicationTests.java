b3f28c0fb62e6ddf474a6351e019dffecd583990
enrique moran linares <enriquemoran.linares@gmail.com>
2022-09-12 14:46:32 +0200

    prueba

---
+++ src/test/java/com/vidoquitest/app/VidoquitestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidoquitest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidoquitestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

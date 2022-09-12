eb8c5ca435b79f1c199f5ea8237b60be9c62af7b
enrique moran linares <enriquemoran.linares@gmail.com>
2022-09-12 18:01:28 +0200

    estructura creada

--- src/main/java/com/vidoquitest/app/hospitalcontroller/HospitalController.java
+++ src/main/java/com/vidoquitest/app/hospitalcontroller/HospitalController.java
@@ -1,5 +1,16 @@
 package com.vidoquitest.app.hospitalcontroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
 public class HospitalController {
+	
+	@Autowired
+
+	
+	
 
 }
